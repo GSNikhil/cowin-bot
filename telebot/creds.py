f5b0202183c20eade06e245ebb0273895a2d5f04
Nikhil G S <gsnikhil333@gmail.com>
2021-06-21 12:00:30 +0530

    Update creds.py

--- telebot/creds.py
+++ telebot/creds.py
@@ -1,3 +1,2 @@
-bot_token = "1828962089:AAH-PXUelxFdGMJjp6-7NOK_TrLarLZO1O4"
-bot_user_name = "CowinBallariBot"
-URL = "https://cowinballaribot.herokuapp.com/"
+bot_token = "YOUR_BOT_TOKEN"
+bot_user_name = "YOUR_BOT_USER_NAME"
